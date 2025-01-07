5ebf4b6c672813b5e7ee2caf37149452c16b450a
seth <107208142+s-eths@users.noreply.github.com>
2025-01-07 21:58:35 +1100

    Update loadstring.lua

--- loadstring.lua
+++ loadstring.lua
@@ -1,2 +1,2 @@
 -- spooky.wtf | made by @s.eths
-loadstring(game:HttpGet(https://raw.githubusercontent.com/s-eths/spooky.wtf/main/main.lua, true))();
+loadstring(game:HttpGet("https://raw.githubusercontent.com/s-eths/spooky.wtf/main/main.lua", true))();
